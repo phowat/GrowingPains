203a834d8f9f8a804c5c5444be0b5bbbda95b53f
Pedro Howat <pedro@instant.com.br>
2017-01-07 01:28:51 -0200

    Initial Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float sizeJumps; 
+
+	void FixedUpdate () {
+
+		Vector3 currentScale = transform.localScale;
+		Vector3 newScale;
+		if (currentScale.x <= 0) {
+			Destroy (gameObject);
+		}
+		if (Input.GetKey ("space")) {
+			newScale = new Vector3 (
+				currentScale.x + sizeJumps,
+				currentScale.y + sizeJumps,
+				currentScale.z + sizeJumps
+			);
+		} else {
+			newScale = new Vector3 (
+				currentScale.x - sizeJumps,
+				currentScale.y - sizeJumps,
+				currentScale.z - sizeJumps
+			);
+		}
+		transform.localScale = newScale;
+	}
+}
